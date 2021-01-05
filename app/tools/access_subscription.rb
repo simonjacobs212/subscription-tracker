a8caf3c7578f8ad6c7cdfad718f8d276ffb127b8
Daniel Sasse <dsasse07@gmail.com>
2021-01-05 09:53:20 -0500

    Access Subscription module re-saved

--- app/tools/access_subscription.rb
+++ app/tools/access_subscription.rb
@@ -11,7 +11,20 @@ module AccessSubscriptions
   end
 
   def subscriptions_action_selection
-
+    choices = ["View all subcriptions", "Change SubscriptionTracker Password", "Back", "Logout"]
+    selection = @@prompt.select("What would you like to do?", choices)
+    case selection
+    when "View all subcriptions"
+      change_app_username_handler
+    when "Change SubscriptionTracker Password"
+      change_app_password_handler
+    when "Back"
+      system 'clear'
+      main_menu
+    when "Logout"
+      system 'clear'
+      run
+    end
   end
 
   
